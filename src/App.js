96e4f8e4a68f32ca8418b19e3617870cf2517595
Raveryk <raveryk@gmail.com>
2021-07-15 20:31:13 -0500

    Added routing to app and boilerplate Home and Sequencer components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import Synth from './Synth/Synth'
+import Sequencer from './Sequencer/Sequencer'
+import Home from './Home/Home'
+
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Link,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      <Synth/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/synth">Synth</Link>
+            </li>
+            <li>
+              <Link to="/sequencer">Sequencer</Link>
+            </li>
+          </ul>
+        </nav>
+      <Switch>
+      <Route
+          exact
+          path="/">
+          <Home/>
+        </Route>
+        <Route
+          exact
+          path="/synth">
+          <Synth/>
+        </Route>
+        <Route
+          exact
+          path="/sequencer">
+          <Sequencer/>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Sequencer/Sequencer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as Tone from "tone";
+import './Sequencer.css';
+
+const Sequencer = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Sequencer
